75270d237adaaa8ce05167f6600d845047f1fa56
Juan Andrew <juandreww108@gmail.com>
2022-01-16 15:27:26 +0700

    boostrap

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,4 +9,14 @@ class AdminController extends Controller
     public function registration(Request $request) {
         return view('admin.registration');
     }
+
+    public function submit(Request $request) {
+        $this->validate($request, [
+            'nama' => 'required|min:5|max:20',
+            'pekerjaan' => 'required',
+            'usia' => 'required|numeric'
+        ]);
+
+        return view('admin.submit',['data' => $request]);
+    }
 }
