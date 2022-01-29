e16067c009f686b67f64879562f4b7a3b1032ee7
Juan Andrew <juandreww108@gmail.com>
2022-01-29 21:13:38 +0700

    ss

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,23 +10,16 @@ use App\Models\UserRegistration;
 class AdminController extends Controller
 {
     public function registration(Request $request) {
-
-        $user = DB::table('trnregistration2')->first();
-        // return response()->json(
-        //     $user,
-        //     Response::HTTP_OK
-        // );
-        return view('admin.registration', ['user' => $user]);
+        return view('admin.registration');
     }
 
     public function submit(Request $request) {
         $this->validate($request, [
-            'nama' => 'required|min:5|max:20',
-            'pekerjaan' => 'required',
-            'usia' => 'required|numeric'
+            'name' => 'required|min:3',
+            'age' => 'required|numeric'
         ]);
-
-        DB::INSERT("INSERT INTO trnregistration2 (Oid, Name, Age) VALUES (UUID(), ?, ?)", [$request->nama, 23]);
+        
+        // DB::INSERT("INSERT INTO trnregistration2 (Oid, Name, Age) VALUES (UUID(), ?, ?)", [$request->nama, 23]);
 
         return view('admin.submit',['data' => $request]);
     }
